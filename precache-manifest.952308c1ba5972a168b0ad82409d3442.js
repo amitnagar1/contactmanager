375ceb58a15bdb7d54acf917e22ec8ca66a5e165
amitnagar1 <amitnagar2264@gmail.com>
2018-10-21 01:58:26 +0200

    Updates

---
+++ precache-manifest.952308c1ba5972a168b0ad82409d3442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "16b18836e5cf19aa8848",
+    "url": "/contactmanager/static/js/main.16b18836.chunk.js"
+  },
+  {
+    "revision": "e07108023900ccf43e2f",
+    "url": "/contactmanager/static/js/1.e0710802.chunk.js"
+  },
+  {
+    "revision": "16b18836e5cf19aa8848",
+    "url": "/contactmanager/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "e07108023900ccf43e2f",
+    "url": "/contactmanager/static/css/1.96514f77.chunk.css"
+  },
+  {
+    "revision": "824a3050c936a0f93acac6f891a7c579",
+    "url": "/contactmanager/index.html"
+  }
+];
